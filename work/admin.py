73d1fad0fc0fa426585f30938777742b713a2a4a
Smdonline <4cVVgJN5dZYUhtM>
2022-03-28 23:30:48 +0200

    add work app to wagtail admin

--- work/admin.py
+++ work/admin.py
@@ -1,9 +1,17 @@
-from django.contrib import admin
+
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin, modeladmin_register)
 from .models import Work
-# Register your models here.
-@admin.register(Work)
-class WorkAdmin(admin.ModelAdmin):
+
+class WorkAdmin(ModelAdmin):
+    model = Work
+    menu_label = 'Works'  # ditch this to use verbose_name_plural from model
+    # menu_icon = 'pilcrow'  # change as required
+    menu_order = 400  # will put in 3rd place (000 being 1st, 100 2nd)
+    add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
+    exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
     list_display = ('name','email','message','phone','created','read','accepted','working','finished')
-    sort_by = ('-created','read')
     list_filter = ['read','accepted']
     search_fields = ('name','email','phone')
+modeladmin_register(WorkAdmin)
+
